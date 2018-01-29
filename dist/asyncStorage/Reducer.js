bd3b32040c4205e6b7e5efb305aaa8b7a027a38e
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-01-29 23:10:38 +1300

    Bug fixed in async storage

--- dist/asyncStorage/Reducer.js
+++ dist/asyncStorage/Reducer.js
@@ -27,25 +27,25 @@ exports.default = function () {
       return state.deleteIn(['failedOperations', action.payload.get('operationId')]);
 
     case _ActionTypes2.default.ASYNC_STORAGE_WRITE_VALUE_SUCCEEDED:
-      return state.updateIn(['writeValueOperationsStatus', action.payload.get('operationId')], _Status2.default.SUCCEEDED).setIn(['keyValues', action.payload.get('key')], action.payload.get('value'));
+      return state.setIn(['writeValueOperationsStatus', action.payload.get('operationId')], _Status2.default.SUCCEEDED).setIn(['keyValues', action.payload.get('key')], action.payload.get('value'));
 
     case _ActionTypes2.default.ASYNC_STORAGE_WRITE_VALUE_FAILED:
-      return state.updateIn(['writeValueOperationsStatus', action.payload.get('operationId')], _Status2.default.FAILED).setIn(['failedOperations', action.payload.get('operationId')], action.payload);
+      return state.setIn(['writeValueOperationsStatus', action.payload.get('operationId')], _Status2.default.FAILED).setIn(['failedOperations', action.payload.get('operationId')], action.payload);
 
     case _ActionTypes2.default.ASYNC_STORAGE_WRITE_VALUE_IN_PROGRESS:
-      return state.updateIn(['writeValueOperationsStatus', action.payload.get('operationId')], _Status2.default.IN_PROGRESS);
+      return state.setIn(['writeValueOperationsStatus', action.payload.get('operationId')], _Status2.default.IN_PROGRESS);
 
     case _ActionTypes2.default.ASYNC_STORAGE_ACKNOWLEDGE_WRITE_VALUE_OPERATION:
       return state.deleteIn(['writeValueOperationsStatus', action.payload.get('operationId')]);
 
     case _ActionTypes2.default.ASYNC_STORAGE_READ_VALUE_SUCCEEDED:
-      return state.updateIn(['readValueOperationsStatus', action.payload.get('operationId')], _Status2.default.SUCCEEDED).setIn(['keyValues', action.payload.get('key')], action.payload.get('value'));
+      return state.setIn(['readValueOperationsStatus', action.payload.get('operationId')], _Status2.default.SUCCEEDED).setIn(['keyValues', action.payload.get('key')], action.payload.get('value'));
 
     case _ActionTypes2.default.ASYNC_STORAGE_READ_VALUE_FAILED:
-      return state.updateIn(['readValueOperationsStatus', action.payload.get('operationId')], _Status2.default.FAILED).deleteIn(['keyValues', action.payload.get('key')]).setIn(['failedOperations', action.payload.get('operationId')], action.payload);
+      return state.setIn(['readValueOperationsStatus', action.payload.get('operationId')], _Status2.default.FAILED).deleteIn(['keyValues', action.payload.get('key')]).setIn(['failedOperations', action.payload.get('operationId')], action.payload);
 
     case _ActionTypes2.default.ASYNC_STORAGE_READ_VALUE_IN_PROGRESS:
-      return state.updateIn(['readValueOperationsStatus', action.payload.get('operationId')], _Status2.default.IN_PROGRESS);
+      return state.setIn(['readValueOperationsStatus', action.payload.get('operationId')], _Status2.default.IN_PROGRESS);
 
     case _ActionTypes2.default.ASYNC_STORAGE_ACKNOWLEDGE_READ_VALUE_OPERATION:
       return state.deleteIn(['readValueOperationsStatus', action.payload.get('operationId')]);
--- src/asyncStorage/Reducer.js
+++ src/asyncStorage/Reducer.js
@@ -11,33 +11,33 @@ export default (state = initialState, action) => {
 
   case ActionTypes.ASYNC_STORAGE_WRITE_VALUE_SUCCEEDED:
     return state
-      .updateIn(['writeValueOperationsStatus', action.payload.get('operationId')], Status.SUCCEEDED)
+      .setIn(['writeValueOperationsStatus', action.payload.get('operationId')], Status.SUCCEEDED)
       .setIn(['keyValues', action.payload.get('key')], action.payload.get('value'));
 
   case ActionTypes.ASYNC_STORAGE_WRITE_VALUE_FAILED:
     return state
-      .updateIn(['writeValueOperationsStatus', action.payload.get('operationId')], Status.FAILED)
+      .setIn(['writeValueOperationsStatus', action.payload.get('operationId')], Status.FAILED)
       .setIn(['failedOperations', action.payload.get('operationId')], action.payload);
 
   case ActionTypes.ASYNC_STORAGE_WRITE_VALUE_IN_PROGRESS:
-    return state.updateIn(['writeValueOperationsStatus', action.payload.get('operationId')], Status.IN_PROGRESS);
+    return state.setIn(['writeValueOperationsStatus', action.payload.get('operationId')], Status.IN_PROGRESS);
 
   case ActionTypes.ASYNC_STORAGE_ACKNOWLEDGE_WRITE_VALUE_OPERATION:
     return state.deleteIn(['writeValueOperationsStatus', action.payload.get('operationId')]);
 
   case ActionTypes.ASYNC_STORAGE_READ_VALUE_SUCCEEDED:
     return state
-      .updateIn(['readValueOperationsStatus', action.payload.get('operationId')], Status.SUCCEEDED)
+      .setIn(['readValueOperationsStatus', action.payload.get('operationId')], Status.SUCCEEDED)
       .setIn(['keyValues', action.payload.get('key')], action.payload.get('value'));
 
   case ActionTypes.ASYNC_STORAGE_READ_VALUE_FAILED:
     return state
-      .updateIn(['readValueOperationsStatus', action.payload.get('operationId')], Status.FAILED)
+      .setIn(['readValueOperationsStatus', action.payload.get('operationId')], Status.FAILED)
       .deleteIn(['keyValues', action.payload.get('key')])
       .setIn(['failedOperations', action.payload.get('operationId')], action.payload);
 
   case ActionTypes.ASYNC_STORAGE_READ_VALUE_IN_PROGRESS:
-    return state.updateIn(['readValueOperationsStatus', action.payload.get('operationId')], Status.IN_PROGRESS);
+    return state.setIn(['readValueOperationsStatus', action.payload.get('operationId')], Status.IN_PROGRESS);
 
   case ActionTypes.ASYNC_STORAGE_ACKNOWLEDGE_READ_VALUE_OPERATION:
     return state.deleteIn(['readValueOperationsStatus', action.payload.get('operationId')]);
